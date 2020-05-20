f06d2e70da400d32a033b766ad697601fbd1da42
oleoot <oleoot28@gmail.com>
2020-05-20 14:30:46 +0300

    Register functionality done

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require('express');
-const expressLayouts = require('express-ejs-layouts')
 const app = express()
+const bcrypt = require('bcryptjs')
+const users = [];
 app.set('view engine', 'ejs')
 
 
 app.get('/', (req, res) => {
     res.render('index.ejs')
 })
-
+app.use(express.urlencoded({ extended: false }))
 app.get('/login', (req, res) => {
     res.render('login.ejs')
 })
@@ -16,12 +17,27 @@ app.get('/register', (req, res) => {
     res.render('register.ejs')
 })
 
-
-app.post('/register', (req, res) => {
+app.post('/login', (req, res) => {
 
 })
-// // EJS
-// app.use(expressLayouts)
+
+
+app.post('/register', async (req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        users.push({
+            id: Date.now().toString(),
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword
+        })
+        res.redirect('/login')
+    } catch{
+        res.redirect('/register')
+    }
+    console.log(users)
+})
+
 
 
 
